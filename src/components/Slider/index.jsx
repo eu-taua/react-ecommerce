73803f9a4ca756b2cc7df40c39a7d57d4cd460cc
taua <taua.a@icloud.com>
2022-08-24 08:41:54 -0300

    feat: add slider component and styles

---
+++ src/components/Slider/index.jsx
@@ -0,0 +1,21 @@
+import bgSlider from "/src/assets/background-slider.png"
+import "./styles.scss"
+
+export const Slider = () => {
+  return (
+    <div className="slider-wrapper">
+      <div className="arrow left center">{"<"}</div>
+      <img src={bgSlider} />
+      <div className="slider-content">
+        <h1>
+          SUMMER <br /> COLECTION
+        </h1>
+        <p>Separamos os melhores looks para você neste 2019</p>
+        <div className="button-wrapper">
+          <button>SHOP NOW</button>
+        </div>
+      </div>
+      <div className="arrow right center">{">"}</div>
+    </div>
+  )
+}
