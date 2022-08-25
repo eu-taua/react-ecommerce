f0f08d289444c62581ede8a8066a602c4e968c80
taua <taua.a@icloud.com>
2022-08-25 04:25:39 -0300

    refactor: header component

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,12 +1,23 @@
-import { Bottom } from "./Bottom"
+import { GiPhone } from "react-icons/gi"
+import { BsBag } from "react-icons/bs"
+
 import "./styles.scss"
 
-import { Top } from "./Top"
-export const Header = props => {
+export const Header = ({ phone = "11 4191 4399", prodAmount, totalValue }) => {
   return (
     <header>
-      <Top {...props} />
-      <Bottom />
+      <div className="phone">
+        <GiPhone />
+        <span>{phone}</span>
+      </div>
+      <span className="logo">AVIATO</span>
+      <div className="bag">
+        <BsBag />
+        <div>
+          <span>({prodAmount}) - </span>
+          <b>R$ {totalValue.toFixed(2)}</b>
+        </div>
+      </div>
     </header>
   )
 }
