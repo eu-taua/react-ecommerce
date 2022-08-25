d56f6057203551e7a0bd33b27e507b6e5dd40c64
taua <taua.a@icloud.com>
2022-08-25 05:21:39 -0300

    refactor: header navbar

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -2,22 +2,64 @@ import { GiPhone } from "react-icons/gi"
 import { BsBag } from "react-icons/bs"
 
 import "./styles.scss"
+import { Link } from "react-router-dom"
 
 export const Header = ({ phone = "11 4191 4399", prodAmount, totalValue }) => {
+  const navLinks = [
+    {
+      to: "/home",
+      name: "HOME",
+    },
+    {
+      to: "/category/camisas",
+      name: "CAMISAS",
+    },
+    {
+      to: "/category/blusas",
+      name: "BLUSAS",
+    },
+    {
+      to: "/category/calcas",
+      name: "CALÇAS",
+    },
+    {
+      to: "/category/acessorios",
+      name: "ACESSÓRIOS",
+    },
+    {
+      to: "/category/outlet",
+      name: "OUTLET",
+    },
+  ]
+  const NavHeader = () => {
+    return (
+      <nav className="header-nav">
+        {navLinks.map(({ to, name }) => (
+          <Link key={name} to={to}>
+            {name}
+          </Link>
+        ))}
+      </nav>
+    )
+  }
+
   return (
     <header>
-      <div className="phone">
-        <GiPhone />
-        <span>{phone}</span>
-      </div>
-      <span className="logo">AVIATO</span>
-      <div className="bag">
-        <BsBag />
-        <div>
-          <span>({prodAmount}) - </span>
-          <b>R$ {totalValue.toFixed(2)}</b>
+      <div className="top">
+        <div className="phone">
+          <GiPhone />
+          <span>{phone}</span>
+        </div>
+        <span className="logo">AVIATO</span>
+        <div className="bag">
+          <BsBag />
+          <div>
+            <span>({prodAmount}) - </span>
+            <b>R$ {totalValue.toFixed(2)}</b>
+          </div>
         </div>
       </div>
+      <NavHeader />
     </header>
   )
 }
