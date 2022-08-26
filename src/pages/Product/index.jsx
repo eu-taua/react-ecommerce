99e71ab5687017afa1f06d5efc7b97fa072c12f6
taua <taua.a@icloud.com>
2022-08-26 04:10:07 -0300

    feat(in progress): product page and styles

--- src/pages/Product/index.jsx
+++ src/pages/Product/index.jsx
@@ -1,3 +1,39 @@
+import { useParams } from "react-router-dom"
+import { ProductList } from "../../components/ProductList"
+import useProducts from "../../hooks/useProducts"
+import "./styles.scss"
+
 export const Product = () => {
-  return <h1>Product</h1>
+  const { products } = useProducts()
+  const { id } = useParams()
+  const product = products.find(prod => prod.id === Number(id))
+  const related = products.slice(0, 4)
+  return (
+    product && (
+      <div className="container">
+        <div className="title-bar-wrapper">
+          <p>Home - Blusas e Camisas - {product.name}</p>
+        </div>
+        <main>
+          <div className="product">
+            <div className="images-wrapper">1</div>
+            <div className="description-wrapper">
+              <p>{product.name}</p>
+              <p>{product.price}</p>
+              <p>
+                But I must explain to you how all this mistaken idea of ouncing and aising pain was born and I will give
+                you a complete count of ut I must explain to you how all this aken idea of enouncing pleasure born and I
+                will give you a complete account of
+              </p>
+            </div>
+          </div>
+
+          <div className="related-products">
+            <h1>Produtos Relacionados</h1>
+            <ProductList products={related} />
+          </div>
+        </main>
+      </div>
+    )
+  )
 }
