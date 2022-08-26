199ebd89eb2ef288946a023d63b36c26931f0614
taua <taua.a@icloud.com>
2022-08-26 00:43:01 -0300

    refactor: remove fetch from app

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
-import { useEffect, useState } from "react"
-import { BrowserRouter, HashRouter, Navigate, Route, Routes } from "react-router-dom"
-import { useHttp } from "./hooks/useHttp"
+import { HashRouter, Navigate, Route, Routes } from "react-router-dom"
 import { Footer } from "./components/Footer"
 import { Header } from "./components/Header"
 import { Home } from "./pages/Home"
@@ -8,26 +6,11 @@ import { Category } from "./pages/Category"
 import { Product } from "./pages/Product"
 import "./styles/global.scss"
 function App() {
-  const [products, setProducts] = useState([])
-
-  useEffect(() => {
-    const fetchProducts = async () => {
-      setProducts(
-        await useHttp(
-          //had to add a proxy at request, the api at heroku hasn't CORS enabled.
-          "https://vast-plateau-19026.herokuapp.com/https://bob-teste-front-end.herokuapp.com/api/products.json"
-        )
-      )
-    }
-    fetchProducts()
-  }, [])
-
   const routes = [
     {
       path: "/home",
       name: "Home Page",
-      element: <Home products={products} />,
-      props: products,
+      element: <Home />,
       exact: true,
     },
     {
