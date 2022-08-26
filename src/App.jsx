ee3025cbfcbf949a8c5d24b39e93a94ed21fbe23
taua <taua.a@icloud.com>
2022-08-26 02:33:15 -0300

    refactor: moving request back to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { useEffect, useState } from "react"
+import { useHttp } from "./hooks/useHttp"
 import { HashRouter, Navigate, Route, Routes } from "react-router-dom"
 import { Footer } from "./components/Footer"
 import { Header } from "./components/Header"
@@ -6,17 +8,31 @@ import { Category } from "./pages/Category"
 import { Product } from "./pages/Product"
 import "./styles/global.scss"
 function App() {
+  const [products, setProducts] = useState([])
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      setProducts(
+        await useHttp(
+          //had to add a proxy at request, the api at heroku hasn't CORS enabled.
+          "https://vast-plateau-19026.herokuapp.com/https://bob-teste-front-end.herokuapp.com/api/products.json"
+        )
+      )
+    }
+    fetchProducts()
+  }, [])
+
   const routes = [
     {
       path: "/home",
       name: "Home Page",
-      element: <Home />,
+      element: <Home products={products} />,
       exact: true,
     },
     {
       path: "/category/:category",
       name: "Category page",
-      element: <Category />,
+      element: <Category products={products} />,
       exact: true,
     },
     {
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,25 +1,9 @@
-import { useEffect, useState } from "react"
-import { useHttp } from "../../hooks/useHttp"
 import { Grid } from "../../components/Grid"
-import { ProductCard } from "../../components/ProductCard"
 import { Slider } from "../../components/Slider"
 import "./styles.scss"
+import { ProductList } from "../../components/ProductList"
 
-export const Home = () => {
-  const [products, setProducts] = useState([])
-
-  useEffect(() => {
-    const fetchProducts = async () => {
-      setProducts(
-        await useHttp(
-          //had to add a proxy at request, the api at heroku hasn't CORS enabled.
-          "https://vast-plateau-19026.herokuapp.com/https://bob-teste-front-end.herokuapp.com/api/products.json"
-        )
-      )
-    }
-    fetchProducts()
-  }, [])
-
+export const Home = ({ products }) => {
   return (
     <div className="home-container">
       <Slider />
@@ -30,12 +14,7 @@ export const Home = () => {
         </section>
         <section className="popular">
           <h1>POPULARES</h1>
-          <div className="products-wrapper">
-            {products &&
-              products.map(prod => {
-                return <ProductCard key={prod.id} product={prod} />
-              })}
-          </div>
+          <ProductList products={products} />
         </section>
         <div className="custom-button">
           <button>VER MAIS PRODUTOS</button>
