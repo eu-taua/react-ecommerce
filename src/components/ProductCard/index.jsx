d9abd11ea5fc1ac08bd5d7a77154caeb81cf747f
taua <taua.a@icloud.com>
2022-08-26 02:43:40 -0300

    feat: add navigation function

--- src/components/ProductCard/index.jsx
+++ src/components/ProductCard/index.jsx
@@ -1,8 +1,10 @@
+import { useNavigate } from "react-router-dom"
 import "./styles.scss"
 
 export const ProductCard = ({ product }) => {
+  const navigate = useNavigate()
   return (
-    <div className="product-wrapper">
+    <div className="product-wrapper" onClick={() => navigate(`/product/${product.id}`)}>
       <img src={product.photo} />
       <div className="description">
         <p>{product.name}</p>
