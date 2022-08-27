e69a89e3736cd076093c2711d754b7f9f18864c3
taua <taua.a@icloud.com>
2022-08-27 17:27:52 -0300

    refactor: wrap product details

--- src/components/ProductCard/index.jsx
+++ src/components/ProductCard/index.jsx
@@ -11,46 +11,49 @@ export const ProductCard = ({ product }) => {
         <img src={product.photo} alt="product image" />
       </div>
       <div className="product-details">
-        <div className="description-wrapper">
-          <h1>{product.name}</h1>
-          <h2>R$ {product.price}</h2>
-          <p>
-            But I must explain to you how all this mistaken idea of ouncing and aising pain was born and I will give you
-            a complete count of ut I must explain to you how all this aken idea of enouncing pleasure born and I will
-            give you a complete account of
-          </p>
-        </div>
-        <ProductOptions selectedOptions={selectedOptions} setSelectedOptions={setSelectedOptions} />
-
-        <div className="buy-product">
-          <div className="product-amount">
-            <button className="dec">-</button>
-            <input
-              value={selectedOptions.amount}
-              onChange={e => setSelectedOptions({ ...selectedOptions, amount: e.target.value })}
-            />
-            <button className="inc">+</button>
-          </div>
-          <div className="custom-button">
-            <button>COMPRAR</button>
-          </div>
-        </div>
-
-        <div className="accordions">
-          <div className="description-accordion">
-            <div className="accordion-header">
-              <h4>DESCRIÇÃO</h4>
-              <h1>-</h1>
-            </div>
+        <div>
+          <div className="description-wrapper">
+            <h1>{product.name}</h1>
+            <h2>R$ {product.price}</h2>
             <p>
-              But I must explain to you how all this mistaken denoun aising pain was born and I will give you a complete
-              account of must explain to you how all this ake pain was born and give{" "}
+              But I must explain to you how all this mistaken idea of ouncing and aising pain was born and I will give
+              you a complete count of ut I must explain to you how all this aken idea of enouncing pleasure born and I
+              will give you a complete account of
             </p>
           </div>
-          <div className="especification-accordion">
-            <div className="accordion-header">
-              <h4>ESPECIFICAÇÕES</h4>
-              <h1>+</h1>
+          <ProductOptions selectedOptions={selectedOptions} setSelectedOptions={setSelectedOptions} />
+        </div>
+        <div>
+          <div className="buy-product">
+            <div className="product-amount">
+              <button className="dec">-</button>
+              <input
+                value={selectedOptions.amount}
+                onChange={e => setSelectedOptions({ ...selectedOptions, amount: e.target.value })}
+              />
+              <button className="inc">+</button>
+            </div>
+            <div className="custom-button">
+              <button>COMPRAR</button>
+            </div>
+          </div>
+
+          <div className="accordions">
+            <div className="description-accordion">
+              <div className="accordion-header">
+                <h4>DESCRIÇÃO</h4>
+                <h1>-</h1>
+              </div>
+              <p>
+                But I must explain to you how all this mistaken denoun aising pain was born and I will give you a
+                complete account of must explain to you how all this ake pain was born and give{" "}
+              </p>
+            </div>
+            <div className="especification-accordion">
+              <div className="accordion-header">
+                <h4>ESPECIFICAÇÕES</h4>
+                <h1>+</h1>
+              </div>
             </div>
           </div>
         </div>
